ee3732380e5ce11676787d7c0b0b9323fa4523b9
Josh Wells <jow102@aber.ac.uk>
2022-03-03 16:09:26 +0000

    updated test1.cpp, started working on image transform to make line tracking easier

--- jow102_mmp/src/test1.cpp
+++ jow102_mmp/src/test1.cpp
@@ -10,6 +10,7 @@
 
 static const std::string OPENCV_WINDOW = "Image window";
 cv_bridge::CvImagePtr cv_ptr;
+using namespace cv;
 
 void image_cb(const sensor_msgs::ImageConstPtr& msg)
 {
@@ -26,12 +27,18 @@ void image_cb(const sensor_msgs::ImageConstPtr& msg)
     // Draw circle of radius 10 at coordinates (0, 0)
     //cv::circle(cv_ptr->image, cv::Point(0, 0), 10, CV_RGB(255,0,0));
 
-    Point2f src[4] = {{225.0, 263.0}}, {248.3, 270.7}, {32.7, 307.7}, {83.0, 334.7}};
-    Point2f dst[4] = {{0.0f, 0.0f}}, {w, 0.0f}, {0.0f, h}, {w, h}};
+    float w = 45, h = 221;
+    Point2f src[4] = {{233, 262},{248, 268},{19, 316},{58, 339}};
+    Point2f dst[4] = {{0.0f, 0.0f},{w, 0.0f},{0.0f, h},{w, h}};
+
+    Mat matrix, imgWarp;
+    matrix = getPerspectiveTransform(src, dst);
+    warpPerspective(cv_ptr->image, imgWarp, matrix, Point(w,h));
 
     // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
+    imshow(OPENCV_WINDOW, cv_ptr->image);
+    imshow("Warped img", imgWarp);
+    waitKey(3);
 
     //cv::imwrite("/impacs/jow102/catkin_ws/src/jow102_mmp/test_img.jpg", cv_ptr->image); //save test image
 }
@@ -47,13 +54,13 @@ int main(int argc, char **argv) {
     image_transport::ImageTransport it(nh);
     image_transport::Subscriber sub = it.subscribe("/camera/rgb/image_raw", 1, image_cb);
     image_transport::Publisher pub = it.advertise("/image_converter/output_video", 1);
-    cv::namedWindow(OPENCV_WINDOW);
+    namedWindow(OPENCV_WINDOW);
 
-    ros::spinOnce();
-    /*while(true){
+    //ros::spinOnce();
+    while(true){
         ros::spinOnce();
         pub.publish(cv_ptr->toImageMsg());
-    }*/
+    }
     return 0;
 }
 

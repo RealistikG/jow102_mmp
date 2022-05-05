501e8c2427fe66648836f7f614f32bd65dd61000
RealistikG <josh.wells41@yahoo.co.uk>
2022-05-05 20:23:12 +0100

    changed some file names

---
+++ jow102_mmp/src/poseLogger.cpp
@@ -0,0 +1,38 @@
+// Include ros lib
+#include <ros/ros.h>
+#include <gazebo_msgs/ModelStates.h>
+#include <iostream>
+#include <fstream>
+
+String name;
+
+void logger_cb(const gazebo_msgs::ModelStates& msg)
+{
+    name = msg.name;
+}
+
+int main(int argc, char **argv) {
+    // Initialize ROS
+    init(argc, argv, "Pose_Logger");
+    NodeHandle mainNh;
+
+    ros::Subscriber sub = mainNh.subscribe("gazebo/model_states", 10, &logger_cb);
+
+    std::ofstream logFile;
+    logFile.open ("poseLog.csv");
+
+    // Loop rate
+    Rate rate(10);
+    // Initial startup
+    spinOnce();
+    rate.sleep();
+
+    // Main loop
+    while(ros::ok())
+    {
+        spinOnce();
+        logFile << "%s", name, endl; //%s, %d", string1, string2, double1 << endl;
+        rate.sleep();
+    }
+    return 0;
+}
